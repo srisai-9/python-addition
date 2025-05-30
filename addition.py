3992167337b5f5e86c94c8464c33279d46571d15
srisai-9 <swapna.chatrasi@gmial.com>
2025-05-30 17:23:41 +0530

    changes

---
+++ addition.py
@@ -0,0 +1,14 @@
+<<<<<<< HEAD
+=======
+#addition of two numbers
+num1=int(input("enter first number"))
+num2=int(input("enter second number"))
+
+result=num1+num2
+
+print("the sum is:", result)
+
+result=num1-num2
+
+print("the sub is:", result)
+>>>>>>> branch1
